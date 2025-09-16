f4bd749c79f7c53467785344f516b346b399619f
Efe Mantaroğlu <efemantaroglum@gmail.com>
2025-09-16 21:57:17 +0300

    feat : add team, player, hall and table tables

--- init.sql
+++ init.sql
@@ -54,7 +54,7 @@ CREATE TABLE `Player` (
   username VARCHAR(255),
   date_of_birth DATE,
   fide_id VARCHAR(70),
-  elo_rating INT,
+  elo_rating INT NOT NULL,
   title_id INT,
   PRIMARY KEY (username),
   FOREIGN KEY (username) REFERENCES User(username),
@@ -62,3 +62,36 @@ CREATE TABLE `Player` (
   CONSTRAINT "Experienced_Player" CHECK (elo_rating > 1000)
 );
 
+
+CREATE TABLE `Team` (
+  team_id INT,
+  team_name VARCHAR(255),
+  coach_username VARCHAR(255) NOT NULL,
+  contract_start date,
+  contract_finish date,
+  PRIMARY KEY (team_id),
+  FOREIGN KEY (coach_username) REFERENCES Coach(username)
+);
+
+CREATE TABLE `PlayerTeam` (
+  username VARCHAR(255),
+  team_id INT,
+  PRIMARY KEY (username, team_id),
+  FOREIGN KEY (username) REFERENCES Player(username),
+  FOREIGN KEY (team_id) REFERENCES Team(team_id)
+)
+
+ CREATE TABLE `Hall` (
+  hall_id INT,
+  hall_name VARCHAR(255),
+  hall_country VARCHAR(255),
+  hall_capacity INT,
+  PRIMARY KEY (hall_id)
+)
+
+ CREATE TABLE `Table` (
+  table_id INT,
+  hall_id INT,
+  PRIMARY KEY (hall_id,table_id),
+  FOREIGN KEY (hall_id) REFERENCES Hall(hall_id)
+)
