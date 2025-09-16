2fdf4bfe1c715cd64ffe5308ec5403ee83902110
Efe Mantaroğlu <efemantaroglum@gmail.com>
2025-09-16 22:14:32 +0300

    feat : add match

--- init.sql
+++ init.sql
@@ -79,7 +79,7 @@ CREATE TABLE `PlayerTeam` (
   PRIMARY KEY (username, team_id),
   FOREIGN KEY (username) REFERENCES Player(username),
   FOREIGN KEY (team_id) REFERENCES Team(team_id)
-)
+);
 
  CREATE TABLE `Hall` (
   hall_id INT,
@@ -87,11 +87,46 @@ CREATE TABLE `PlayerTeam` (
   hall_country VARCHAR(255),
   hall_capacity INT,
   PRIMARY KEY (hall_id)
-)
+);
 
  CREATE TABLE `Table` (
   table_id INT,
   hall_id INT,
   PRIMARY KEY (hall_id,table_id),
   FOREIGN KEY (hall_id) REFERENCES Hall(hall_id)
-)
+);
+
+CREATE TABLE `Match` (
+  match_id int,
+  hall_id int,
+  table_id int,
+  white_player_team int,
+  white_player varchar(255),
+  black_player_team int,
+  black_player varchar(255),
+  result varchar(10),
+  time_slot int DEFAULT NULL,
+  date date DEFAULT NULL,
+  assigned_arbiter_username varchar(255) DEFAULT NULL,
+  rating int DEFAULT NULL,
+  creator_coach_username varchar(255) DEFAULT NULL,
+  PRIMARY KEY (match_id),
+  FOREIGN KEY (hall_id) REFERENCES Hall(hall_id),
+  FOREIGN KEY (hall_id,table_id) REFERENCES Table(hall_id,table_id),
+  FOREIGN KEY (white_player_team) REFERENCES Team(team_id),
+  FOREIGN KEY (white_player) REFERENCES Player(username),
+  CONSTRAINT White_Player_In_Team FOREIGN KEY (white_player,white_player_team) REFERENCES PlayerTeam(username,team_id),
+  FOREIGN KEY (black_player_team) REFERENCES Team(team_id),
+  FOREIGN KEY (black_player) REFERENCES Player(username),
+  CONSTRAINT Black_Player_In_Team FOREIGN KEY (black_player,black_player_team) REFERENCES PlayerTeam(username,team_id),
+  CONSTRAINT Valid_Result CHECK (result IN ("White","Black","Draw")),
+  CONSTRAINT Valid_Timeslot CHECK (time_slot >= 1 AND time_slot <= 3),
+  FOREIGN KEY (assigned_arbiter_username) REFERENCES Arbiter(username),
+  CONSTRAINT Valid_Rating CHECK (rating >= 1 AND rating <= 10),
+  FOREIGN KEY (creator_coach_username) REFERENCES Coach(username),
+  UNIQUE KEY Location_Conflict (hall_id,table_id,time_slot,date) ,
+  UNIQUE KEY Black_Conflict (black_player,time_slot,date),
+  UNIQUE KEY White_Conflict (white_player,time_slot,date),
+  UNIQUE KEY Arbiter_Conflict (assigned_arbiter_username,time_slot,date),
+  CONSTRAINT Different_Teams CHECK (black_player_team <> white_player_team)
+);
